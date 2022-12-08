5db7a1696cb599c8b6abda9d2da1f1ffca076c82
Derek Ahn <git.derek@gmail.com>
2022-12-08 13:08:59 -0800

    test: fix subscribe_returns_a_200_for_valid_form_data

--- tests/health_check.rs
+++ tests/health_check.rs
@@ -1,10 +1,13 @@
 use std::net::TcpListener;
 
+use sqlx::{Connection, PgConnection};
+use zero2prod::{configuration::get_configuration, startup};
+
 fn spawn_app() -> String {
     let listener = TcpListener::bind("127.0.0.1:0").expect("Failed to bind random port");
 
     let port = listener.local_addr().unwrap().port();
-    let server = zero2prod::run(listener).expect("failed to bind address");
+    let server = startup::run(listener).expect("failed to bind address");
     let _ = tokio::spawn(server);
 
     format!("http://127.0.0.1:{port}")
@@ -28,6 +31,13 @@ async fn health_check_works() {
 #[tokio::test]
 async fn subscribe_returns_a_200_for_valid_form_data() {
     let app_address = spawn_app();
+    let configuration = get_configuration().expect("Failed to read configuration");
+    let url = configuration.database.connection_string();
+
+    let _connection = PgConnection::connect(&url)
+        .await
+        .expect("Failed to connect to Postgres");
+
     let client = reqwest::Client::new();
 
     let body = "name=le%20guin&email=ursula_le_guin%40gmail.com";
