0aa06b6897634baabac1a7dc0c06a28309c8a199
Derek Ahn <git.derek@gmail.com>
2022-12-08 21:23:13 -0800

    refactor: switch from a shared PgConnection to PgPool - PgPool is a pool of conns bypassing the concurrency issue with PgConnection - PgPool still has interior mutability - PgPool will borrow a PgConnection from the pool and use it to exec the   query; if no connection is available it will create a new one or wait   until one is free

--- src/routes/subscriptions.rs
+++ src/routes/subscriptions.rs
@@ -1,8 +1,8 @@
 use actix_web::{web, HttpResponse};
 
 use chrono::Utc;
-use uuid::Uuid;
 use sqlx::PgPool;
+use uuid::Uuid;
 
 #[derive(serde::Deserialize)]
 pub struct FormData {
@@ -10,22 +10,24 @@ pub struct FormData {
     name: String,
 }
 
-pub async fn subscribe(
-    form: web::Form<FormData>,
-    pool: web::Data<PgPool>,
-) -> HttpResponse {
-    sqlx::query!(
+pub async fn subscribe(form: web::Form<FormData>, pool: web::Data<PgPool>) -> HttpResponse {
+    match sqlx::query!(
         r#"
-        INSERT INTO subscriptions (id, email, name, subscribed_at)
-        VALUES ($1, $2, $3, $4)
+    INSERT INTO subscriptions (id, email, name, subscribed_at)
+    VALUES ($1, $2, $3, $4)
         "#,
         Uuid::new_v4(),
         form.email,
         form.name,
         Utc::now()
     )
-    .execute(pool.get_ref())
-    .await;
-
-    HttpResponse::Ok().finish()
+    .execute(pool.as_ref())
+    .await
+    {
+        Ok(_) => HttpResponse::Ok().finish(),
+        Err(e) => {
+            println!("Failed to execute query: {}", e);
+            HttpResponse::InternalServerError().finish()
+        }
+    }
 }
