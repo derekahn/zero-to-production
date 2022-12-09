8a63404a3a2e547b7ef319c1382f499b2c471ac6
Derek Ahn <git.derek@gmail.com>
2022-12-08 20:46:02 -0800

    fix: use sqlx::PgPool for executor

--- src/routes/subscriptions.rs
+++ src/routes/subscriptions.rs
@@ -1,6 +1,8 @@
-#![allow(dead_code)]
-
 use actix_web::{web, HttpResponse};
+
+use chrono::Utc;
+use uuid::Uuid;
+use sqlx::PgPool;
 
 #[derive(serde::Deserialize)]
 pub struct FormData {
@@ -8,6 +10,22 @@ pub struct FormData {
     name: String,
 }
 
-pub async fn subscribe(_form: web::Form<FormData>) -> HttpResponse {
+pub async fn subscribe(
+    form: web::Form<FormData>,
+    pool: web::Data<PgPool>,
+) -> HttpResponse {
+    sqlx::query!(
+        r#"
+        INSERT INTO subscriptions (id, email, name, subscribed_at)
+        VALUES ($1, $2, $3, $4)
+        "#,
+        Uuid::new_v4(),
+        form.email,
+        form.name,
+        Utc::now()
+    )
+    .execute(pool.get_ref())
+    .await;
+
     HttpResponse::Ok().finish()
 }
