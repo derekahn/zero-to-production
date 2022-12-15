1ed3d13f40c3cc7b2e38ecdc0a650d8f2ab1b832
Derek Ahn <git.derek@gmail.com>
2022-12-14 17:25:51 -0800

    refactor: use wiring tracing in main.rs

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,33 @@
 use std::net::TcpListener;
 
-use env_logger::{Builder, Env};
 use sqlx::PgPool;
+
+use tracing::subscriber::set_global_default;
+use tracing_bunyan_formatter::{BunyanFormattingLayer, JsonStorageLayer};
+use tracing_subscriber::{layer::SubscriberExt, EnvFilter, Registry};
+use tracing_log::LogTracer;
 
 use zero2prod::configuration::get_configuration;
 use zero2prod::startup::run;
 
 #[tokio::main]
 async fn main() -> std::io::Result<()> {
-    Builder::from_env(Env::default().default_filter_or("info")).init();
+    LogTracer::init().expect("Failed to get logger");
+
+    let env_filter = EnvFilter::try_from_default_env()
+        .unwrap_or_else(|_| EnvFilter::new("info"));
+
+    let formatting_layer = BunyanFormattingLayer::new(
+        "zero2prod".into(),
+        std::io::stdout,
+    );
+
+    let subscriber = Registry::default()
+        .with(env_filter)
+        .with(JsonStorageLayer)
+        .with(formatting_layer);
+
+    set_global_default(subscriber).expect("Failed to set subscriber");
 
     let configuration = get_configuration().expect("Failed to read configuration");
 
