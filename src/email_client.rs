63261c05cf64f21d7c045535b44ea697339585ab
Derek Ahn <git.derek@gmail.com>
2022-12-17 22:17:45 -0800

    test: add send_email_succeeds_if_the_server_returns_200

--- src/email_client.rs
+++ src/email_client.rs
@@ -100,13 +100,31 @@ mod tests {
     }
 
     #[tokio::test]
+    async fn send_email_succeeds_if_the_server_returns_200() {
         let mock_server = MockServer::start().await;
-        let sender = SubscriberEmail::parse(SafeEmail().fake()).unwrap();
-        let email_client = EmailClient::new(mock_server.uri(), sender, Secret::new(Faker.fake()));
+
+        let email_client = EmailClient::new(
+            mock_server.uri(),
+            SubscriberEmail::parse(SafeEmail().fake()).unwrap(),
+            Secret::new(Faker.fake()),
+        );
+
+        Mock::given(any())
+            .respond_with(ResponseTemplate::new(200))
+            .expect(1)
+            .mount(&mock_server)
+            .await;
 
         let subscriber_email = SubscriberEmail::parse(SafeEmail().fake()).unwrap();
         let subject: String = Sentence(1..2).fake();
         let content: String = Paragraph(1..10).fake();
+
+        let outcome = email_client
+            .send_email(subscriber_email, &subject, &content, &content)
+            .await;
+
+        assert_ok!(outcome);
+    }
 
     #[tokio::test]
     async fn send_email_sends_the_expected_request() {
