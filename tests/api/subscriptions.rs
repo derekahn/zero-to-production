7451bfcd7184276e357a8614d592381f677411f8
Derek Ahn <git.derek@gmail.com>
2022-12-23 13:21:24 -0800

    test: refactor to use TestApp.get_confirmation_links

--- tests/api/subscriptions.rs
+++ tests/api/subscriptions.rs
@@ -95,7 +95,7 @@ async fn subscribe_sends_a_confirmation_email_for_valid_data() {
 }
 
 #[tokio::test]
-async fn subscribe_sends_a_confrimation_email_with_a_link() {
+async fn subscribe_sends_a_confirmation_email_with_a_link() {
     let app = spawn_app().await;
     let body = "name=le%20guin&email=ursula_le_guin%40gmail.com";
 
@@ -108,18 +108,7 @@ async fn subscribe_sends_a_confrimation_email_with_a_link() {
     app.post_subscriptions(body.into()).await;
 
     let email_request = &app.email_server.received_requests().await.unwrap()[0];
-    let body: serde_json::Value = serde_json::from_slice(&email_request.body).unwrap();
+    let confirmation_links = app.get_confirmation_links(&email_request);
 
-    let get_link = |s: &str| {
-        let links: Vec<_> = linkify::LinkFinder::new()
-            .links(s)
-            .filter(|l| *l.kind() == linkify::LinkKind::Url)
-            .collect();
-
-        links[0].as_str().to_owned()
-    };
-
-    let html_link = get_link(&body["HtmlBody"].as_str().unwrap());
-    let text_link = get_link(&body["TextBody"].as_str().unwrap());
-    assert_eq!(html_link, text_link);
+    assert_eq!(confirmation_links.html, confirmation_links.plain_text);
 }
