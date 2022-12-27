c8c49f42882f5e96e5b019b3b897692856aa07d3
Derek Ahn <git.derek@gmail.com>
2022-12-26 20:28:34 -0800

    refactor: 8.4 Avoid "Ball Of Mud" Error Enums

--- src/routes/subscriptions.rs
+++ src/routes/subscriptions.rs
@@ -14,27 +14,15 @@ use crate::{
 pub enum SubscribeError {
     #[error("{0}")]
     ValidationError(String),
-    #[error("Failed to acquire a Postgres connection form the pool")]
-    PoolError(sqlx::Error),
-    #[error("Failed to insert new subscriber in the database")]
-    InsertSubscriberError(sqlx::Error),
-    #[error("Failed to store the confirmation token for a new subscriber")]
-    StoreTokenError(StoreTokenError),
-    #[error("Failed to commit SQL transaction to store a new subscriber")]
-    TransactionCommitError(sqlx::Error),
-    #[error("Failed to send a confirmation email")]
-    SendEmailError(reqwest::Error),
+    #[error("{1}")]
+    UnexpectedError(#[source] Box<dyn std::error::Error>, String),
 }
 
 impl ResponseError for SubscribeError {
     fn status_code(&self) -> reqwest::StatusCode {
         match self {
             SubscribeError::ValidationError(_) => StatusCode::BAD_REQUEST,
-            SubscribeError::PoolError(_)
-            | SubscribeError::TransactionCommitError(_)
-            | SubscribeError::InsertSubscriberError(_)
-            | SubscribeError::StoreTokenError(_)
-            | SubscribeError::SendEmailError(_) => StatusCode::INTERNAL_SERVER_ERROR,
+            SubscribeError::UnexpectedError(_, _) => StatusCode::INTERNAL_SERVER_ERROR,
         }
     }
 }
@@ -42,24 +30,6 @@ impl ResponseError for SubscribeError {
 impl std::fmt::Debug for SubscribeError {
     fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
         error_chain_fmt(self, f)
-    }
-}
-
-impl From<reqwest::Error> for SubscribeError {
-    fn from(e: reqwest::Error) -> Self {
-        Self::SendEmailError(e)
-    }
-}
-
-impl From<StoreTokenError> for SubscribeError {
-    fn from(e: StoreTokenError) -> Self {
-        Self::StoreTokenError(e)
-    }
-}
-
-impl From<String> for SubscribeError {
-    fn from(e: String) -> Self {
-        Self::ValidationError(e)
     }
 }
 
@@ -105,18 +75,38 @@ pub async fn subscribe(
 ) -> Result<HttpResponse, SubscribeError> {
     let new_subscriber = form.0.try_into().map_err(SubscribeError::ValidationError)?;
     let subscription_token = generate_subscription_token();
-    let mut transaction = pool.begin().await.map_err(SubscribeError::PoolError)?;
+
+    let mut transaction = pool.begin().await.map_err(|e| {
+        SubscribeError::UnexpectedError(
+            Box::new(e),
+            "Failed to acquire a Postgres connection from the pool".into(),
+        )
+    })?;
 
     let subscriber_id = insert_subscriber(&mut transaction, &new_subscriber)
         .await
-        .map_err(SubscribeError::InsertSubscriberError)?;
+        .map_err(|e| {
+            SubscribeError::UnexpectedError(
+                Box::new(e),
+                "Failed to insert new subscriber in the database".into(),
+            )
+        })?;
 
-    store_token(&mut transaction, subscriber_id, &subscription_token).await?;
-
-    transaction
-        .commit()
+    store_token(&mut transaction, subscriber_id, &subscription_token)
         .await
-        .map_err(SubscribeError::TransactionCommitError)?;
+        .map_err(|e| {
+            SubscribeError::UnexpectedError(
+                Box::new(e),
+                "Failed to store the confirmation token for a new subscriber".into(),
+            )
+        })?;
+
+    transaction.commit().await.map_err(|e| {
+        SubscribeError::UnexpectedError(
+            Box::new(e),
+            "Failed to commit SQL transaction to store a new subscriber".into(),
+        )
+    })?;
 
     send_confirmation_email(
         &email_client,
@@ -124,7 +114,10 @@ pub async fn subscribe(
         &base_url.0,
         &subscription_token,
     )
-    .await?;
+    .await
+    .map_err(|e| {
+        SubscribeError::UnexpectedError(Box::new(e), "Failed to send a confirmation email".into())
+    })?;
 
     Ok(HttpResponse::Ok().finish())
 }
