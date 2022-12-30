7079e3505dffc816e6d94dd927b1ffc8d42084c4
Derek Ahn <git.derek@gmail.com>
2022-12-29 20:05:35 -0800

    refactor: 9.8.2 Follow The Compiler - changed send_email to take a reference instead of ownership of   recipient - updated all invocations to use a reference

--- src/email_client.rs
+++ src/email_client.rs
@@ -42,7 +42,7 @@ impl EmailClient {
 
     pub async fn send_email(
         &self,
-        recipient: SubscriberEmail,
+        recipient: &SubscriberEmail,
         subject: &str,
         html_content: &str,
         text_content: &str,
@@ -145,7 +145,7 @@ mod tests {
 
         let email_client = email_client(mock_server.uri());
         let outcome = email_client
-            .send_email(email(), &subject(), &content(), &content())
+            .send_email(&email(), &subject(), &content(), &content())
             .await;
 
         assert_err!(outcome);
@@ -162,7 +162,7 @@ mod tests {
 
         let email_client = email_client(mock_server.uri());
         let outcome = email_client
-            .send_email(email(), &subject(), &content(), &content())
+            .send_email(&email(), &subject(), &content(), &content())
             .await;
 
         assert_err!(outcome);
@@ -179,7 +179,7 @@ mod tests {
 
         let email_client = email_client(mock_server.uri());
         let outcome = email_client
-            .send_email(email(), &subject(), &content(), &content())
+            .send_email(&email(), &subject(), &content(), &content())
             .await;
 
         assert_ok!(outcome);
@@ -200,7 +200,7 @@ mod tests {
 
         let email_client = email_client(mock_server.uri());
         let _ = email_client
-            .send_email(email(), &subject(), &content(), &content())
+            .send_email(&email(), &subject(), &content(), &content())
             .await;
     }
 }
