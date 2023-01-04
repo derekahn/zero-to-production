654c6268a885a441b2bf410149ba38aad353f6fb
Derek Ahn <git.derek@gmail.com>
2023-01-04 12:56:52 -0800

    feat: 10.6.1 HTML Forms

--- src/routes/login/get.rs
+++ src/routes/login/get.rs
@@ -1,7 +1,7 @@
 use actix_web::{http::header::ContentType, HttpResponse};
 
-pub async fn home() -> HttpResponse {
+pub async fn login_form() -> HttpResponse {
     HttpResponse::Ok()
         .content_type(ContentType::html())
-        .body(include_str!("home.html"))
+        .body(include_str!("login.html"))
 }
