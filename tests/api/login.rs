41ae8ef91906f6cfb3d371818b8f33017e322937
Derek Ahn <git.derek@gmail.com>
2023-01-07 20:59:05 -0800

    test: 10.6.4.11 An Integration Test For Login Failures Pt2

--- tests/api/login.rs
+++ tests/api/login.rs
@@ -5,7 +5,7 @@ use reqwest::header::HeaderValue;
 use crate::helpers::{assert_is_redirect_to, spawn_app};
 
 #[tokio::test]
-async fn an_error_flash_message() {
+async fn an_error_flash_message_is_set_on_failure() {
     let app = spawn_app().await;
     let login_body = serde_json::json!({
         "username": "random-username",
@@ -25,4 +25,7 @@ async fn an_error_flash_message() {
 
     let flash_cookie = response.cookies().find(|c| c.name() == "_flash").unwrap();
     assert_eq!(flash_cookie.value(), "Authentication failed");
+
+    let html_page = app.get_login_html().await;
+    assert!(html_page.contains("<p><i>Authentication failed</i></p>"));
 }
