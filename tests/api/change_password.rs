189bdfd5ee8a9b4d29c59c6ba82d943376f1e793
Derek Ahn <git.derek@gmail.com>
2023-01-08 21:18:46 -0800

    feat: 10.8.2.3 Unhappy Path: The Current Password Is Invalid

--- tests/api/change_password.rs
+++ tests/api/change_password.rs
@@ -56,3 +56,32 @@ async fn new_password_field_must_match() {
          the field values must match</i></p>"
     ));
 }
+
+#[tokio::test]
+async fn current_password_must_be_valid() {
+    let app = spawn_app().await;
+
+    // Login
+    app.post_login(&serde_json::json!({
+        "username": &app.test_user.username,
+        "password": &app.test_user.password
+    }))
+    .await;
+
+    let new_password = Uuid::new_v4().to_string();
+    let wrong_password = Uuid::new_v4().to_string();
+
+    // Try to change password
+    let response = app
+        .post_change_password(&serde_json::json!({
+            "current_password": &wrong_password,
+            "new_password": &new_password,
+            "new_password_check": &new_password,
+        }))
+        .await;
+    assert_is_redirect_to(&response, "/admin/password");
+
+    // Follow the redirect
+    let html_page = app.get_change_password_html().await;
+    assert!(html_page.contains("<p><i>The current password is incorrect</i></p>"));
+}
